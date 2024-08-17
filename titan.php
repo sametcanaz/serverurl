088ecbfd9a93fd5b6e4f798e4b24d0654569657b
sametcanaz <151410083+sametcanaz@users.noreply.github.com>
2024-08-17 14:35:15 +0300

    Update titan.php

--- titan.php
+++ titan.php
@@ -1,10 +1,8 @@
 #EXTM3U
 #EXT-X-VERSION:3
-#EXT-X-MEDIA-SEQUENCE:2321
+#EXT-X-MEDIA-SEQUENCE:2322
 #EXT-X-ALLOW-CACHE:YES
 #EXT-X-TARGETDURATION:10
-#EXTINF:10.000000,
-https://maxproizliyorum.serv00.net/corsmaxpro.php/http://66bf9b4bec4da.wz0ju4tbd9jiqmw.xyz:8080/hls/40cd7060504f2c680dad1f1833818f29/342457_2321.ts?maxprotv_calaninaminakorum.sc
 #EXTINF:10.000000,
 https://maxproizliyorum.serv00.net/corsmaxpro.php/http://66bf9b4bec4da.wz0ju4tbd9jiqmw.xyz:8080/hls/641d9c59079da7dc1079f70a84de1bcb/342457_2322.ts?maxprotv_calaninaminakorum.sc
 #EXTINF:10.000000,
@@ -15,3 +13,5 @@ https://maxproizliyorum.serv00.net/corsmaxpro.php/http://66bf9b4bec4da.wz0ju4tbd
 https://maxproizliyorum.serv00.net/corsmaxpro.php/http://66bf9b4bec4da.wz0ju4tbd9jiqmw.xyz:8080/hls/439990b0ecc5b8806b54c815a5fd6866/342457_2325.ts?maxprotv_calaninaminakorum.sc
 #EXTINF:10.000000,
 https://maxproizliyorum.serv00.net/corsmaxpro.php/http://66bf9b4bec4da.wz0ju4tbd9jiqmw.xyz:8080/hls/08b9ba18811f4de0bb60d1695a74dc32/342457_2326.ts?maxprotv_calaninaminakorum.sc
+#EXTINF:10.000000,
+https://maxproizliyorum.serv00.net/corsmaxpro.php/http://66bf9b4bec4da.wz0ju4tbd9jiqmw.xyz:8080/hls/16a17436301328a9805d42e3eac4bf14/342457_2327.ts?maxprotv_calaninaminakorum.sc
